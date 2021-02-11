94cb503345648de6ceb9592f7db4446bea5d6964
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-11 16:10:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function buttonOn() {
+  document.getElementById('paragraph').innerHTML = 'My Melody'
+  document.getElementById('sanrio').src = './Images/mymelody.jpg'
+  document.getElementById('background').style.backgroundColor = 'pink'
+  document.getElementById('paragraph').style.color = 'brown'
+  document.getElementById('paragraph').style.fontFamily = 'Impact,Charcoal,sans-serif'
+  document.getElementById ('paragraph').style.fontSize = '25px'
+}
